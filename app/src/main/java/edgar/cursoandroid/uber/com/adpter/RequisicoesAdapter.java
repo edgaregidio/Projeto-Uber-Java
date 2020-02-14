db1a7c8faadbf20dad7aff6ac81d9c284376ff05
edgaregidio <edgarsiqueirasi@gmail.com>
2020-02-14 14:57:42 -0300

    Projeto Uber

---
+++ app/src/main/java/edgar/cursoandroid/uber/com/adpter/RequisicoesAdapter.java
@@ -0,0 +1,83 @@
+package edgar.cursoandroid.uber.com.adpter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+import edgar.cursoandroid.uber.com.R;
+import edgar.cursoandroid.uber.com.helper.Local;
+import edgar.cursoandroid.uber.com.model.Requisicao;
+import edgar.cursoandroid.uber.com.model.Usuario;
+
+public class RequisicoesAdapter extends RecyclerView.Adapter<RequisicoesAdapter.MyViewHolder> {
+
+    private List<Requisicao> requisicoes;
+    private Context context;
+    private Usuario motorista;
+
+    public RequisicoesAdapter(List<Requisicao> requisicoes, Context context, Usuario motorista) {
+        this.requisicoes = requisicoes;
+        this.context = context;
+        this.motorista = motorista;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_requisicoes, parent, false);
+        return new MyViewHolder( item ) ;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Requisicao requisicao = requisicoes.get( position );
+        Usuario passageiro = requisicao.getPassageiro();
+
+        holder.nome.setText( passageiro.getNome() );
+
+        if(motorista!= null){
+
+            LatLng localPassageiro = new LatLng(
+                    Double.parseDouble(passageiro.getLatitude()),
+                    Double.parseDouble(passageiro.getLongitude())
+            );
+
+            LatLng localMotorista = new LatLng(
+                    Double.parseDouble(motorista.getLatitude()),
+                    Double.parseDouble(motorista.getLongitude())
+            );
+            float distancia = Local.calcularDistancia(localPassageiro, localMotorista);
+            String distanciaFormatada = Local.formatarDistancia(distancia);
+            holder.distancia.setText(distanciaFormatada + "- aproximadamente");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return requisicoes.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nome, distancia;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            nome = itemView.findViewById(R.id.textRequisicaoNome);
+            distancia = itemView.findViewById(R.id.textRequisicaoDistancia);
+
+        }
+    }
+
+
+
+}
